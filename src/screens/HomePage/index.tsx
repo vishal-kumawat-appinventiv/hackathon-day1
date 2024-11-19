2eefabbabc1122def15ab723a027383d2a26f727
vishal-kumawat-appinventiv <vishal.kumawat@appinventiv.com>
2024-11-19 11:26:55 +0530

    homescreen ui

--- src/screens/HomePage/index.tsx
+++ src/screens/HomePage/index.tsx
@@ -1,5 +1,33 @@
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { SquarePlus } from "lucide-react";
 const HomePage = () => {
-  return <div className="text-3xl">HomePage</div>;
+  return (
+    <div className="max-w-3xl mx-auto mt-12">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <Card className="hover:cursor-pointer">
+          <CardHeader>
+            <CardTitle className="flex items-center justify-between w-full">
+              Add Income <SquarePlus size={30} />
+            </CardTitle>
+            <CardDescription>Add the income details.</CardDescription>
+          </CardHeader>
+        </Card>
+        <Card className="hover:cursor-pointer">
+          <CardHeader>
+            <CardTitle className="flex items-center justify-between w-full">
+              Add Expense <SquarePlus size={30} />
+            </CardTitle>
+            <CardDescription>Add the expense details.</CardDescription>
+          </CardHeader>
+        </Card>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
