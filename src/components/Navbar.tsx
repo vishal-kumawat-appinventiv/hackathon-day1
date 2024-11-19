154ce12fd290ca57a4f0933718866e45df55176e
vishal-kumawat-appinventiv <vishal.kumawat@appinventiv.com>
2024-11-19 16:34:00 +0530

    navbar ui

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,20 @@
+import { useNavigate } from "react-router-dom";
 import { ModeToggle } from "./mode-toggle";
 
 const Navbar = () => {
+  const navigate = useNavigate();
   return (
-    <div className="p-2 flex items-center justify-between bg-gray-100 dark:bg-background">
-      <div>
-        <h1 className="text-2xl">Expense Tracker</h1>
-      </div>
-      <div className="hidden md:block pr-14">
-        <p>Made by Vishal Kumawat for Appinventiv Hackathon</p>
-      </div>
-      <div>
-        <ModeToggle />
+    <div className="bg-gray-100 dark:bg-background">
+      <div className="max-w-6xl mx-auto p-4 flex items-center justify-between">
+        <div onClick={() => navigate("/")} className="cursor-pointer">
+          <h1 className="text-2xl">Expense Tracker</h1>
+        </div>
+        <div className="hidden md:block pr-14">
+          <p>Made by Vishal Kumawat for Appinventiv Hackathon</p>
+        </div>
+        <div>
+          <ModeToggle />
+        </div>
       </div>
     </div>
   );
