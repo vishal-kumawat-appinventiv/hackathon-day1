e645cda596f4a76ae71a832c4b307d463abf42e0
vishal-kumawat-appinventiv <vishal.kumawat@appinventiv.com>
2024-11-19 16:29:10 +0530

    table added in income

--- src/screens/IncomePage/index.tsx
+++ src/screens/IncomePage/index.tsx
@@ -1,13 +1,18 @@
 import { getIncomeData } from "@/redux/slices/budgetSlice";
 import { useSelector } from "react-redux";
+import { columns } from "./columns";
+import { DataTable } from "./data-table";
 
 const IncomePage = () => {
-
   const incomeData = useSelector(getIncomeData);
-
-  console.log(incomeData);
-
-  return <div>IncomePage</div>;
+  return (
+    <>
+      <div className="max-w-6xl mx-auto mt-5 md:mt-10 p-4">
+        <h1 className="text-2xl mb-5">Income Management Page</h1>
+        <DataTable columns={columns} data={incomeData} />
+      </div>
+    </>
+  );
 };
 
 export default IncomePage;
